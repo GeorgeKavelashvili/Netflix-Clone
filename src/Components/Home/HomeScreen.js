b6451a77073111b72a67954dee3f691cc6ac96df
GeoulMan <gkavelashvili2@gmail.com>
2023-02-12 00:17:32 +0400

    nav animation done

--- src/Components/Home/HomeScreen.js
+++ src/Components/Home/HomeScreen.js
@@ -1,6 +1,36 @@
 import React from "react";
 import "./HomeScreen.css";
+import Nav from "../Nav/Nav";
 function HomeScreen() {
-  return <div className="homeScreen">netflix</div>;
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+      <h1>TEst</h1>
+    </div>
+  );
 }
 export default HomeScreen;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 function Nav() {
-  return <div className="nav">netflix</div>;
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_content">
+        <img
+          className="nav_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        />
+        <img
+          className="nav_avatar"
+          src="https://www.pngarts.com/files/11/Avatar-Free-PNG-Image.png"
+        />
+      </div>
+    </div>
+  );
 }
 export default Nav;
