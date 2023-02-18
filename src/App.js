ce16652cb85d376d517ce7d840a7441d4624eef8
GeoulMan <gkavelashvili2@gmail.com>
2023-02-18 20:46:54 +0400

    implemented redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/Home/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 function App() {
   const user = null;
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        console.log(userAuth);
+      } else {
+        //logged out
+      }
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
       <Router>
@@ -20,6 +32,7 @@ function App() {
   );
 }
 export default App;
+
 //tu ginda ro homepage naxo essaa
 // export default App;
 // import React from "react";
