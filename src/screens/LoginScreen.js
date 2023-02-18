0db10a80112f8f7856b4e62139a9b00990a255ee
GeoulMan <gkavelashvili2@gmail.com>
2023-02-18 18:05:49 +0400

    done starting page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./LoginScreen.css";
+import SignInScreen from "./SignInScreen";
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
         <img
           className="loginScreen_logo"
-          src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         />
+        <button onClick={() => setSignIn(true)} className="loginScreen_button">
+          Sign In
+        </button>
+        <div className="loginScreen_gradient" />
+      </div>
+      <div className="loginScreen_body">
+        {signIn ? (
+          <SignInScreen />
+        ) : (
+          <>
+            <h1>Unlimited Films,TV programmes and more</h1>
+            <h2>Watch anywhere.Cancel at any time</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen_getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignInScreen() {
+  return <div className="signupScreen"></div>;
+}
+
+export default SignInScreen;
